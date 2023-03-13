80f97631638f7f514e05a4f461cc4a00c85eb9b5
cse-child <99868973+cse-child@users.noreply.github.com>
2023-03-13 22:03:11 +0900

    # 230313 study

--- Study_Coding_Test/Programmers/Level2/위장.cpp
+++ Study_Coding_Test/Programmers/Level2/위장.cpp
@@ -1,16 +1,15 @@
+// https://school.programmers.co.kr/questions/31757
+
 #include <string>
 #include <vector>
 #include <unordered_map>
-#include <iostream>
-#include <algorithm>
+#include <map>
 
 using namespace std;
 
-// https://school.programmers.co.kr/questions/31757
-
-int combination(int n, int r) // nCr = n!/(n-r)!r!
+long long combination(int n, int r) // nCr = n!/(n-r)!r!
 {
-	int result = 1;
+	long long result = 1;
 
 	for (int i = 0; i < n; i++)
 		result *= (n - i);
@@ -20,29 +19,22 @@ int combination(int n, int r) // nCr = n!/(n-r)!r!
 
 	for (int i = 0; i < r; i++)
 		result /= (r - i);
-	
+
 	return result;
 }
 
 int solution(vector<vector<string>> clothes) {
-	unordered_map<string, int> divideCloth;
+	map<string, int> divideCloth;
 
 	for (vector<string> cloth : clothes)
 		divideCloth[cloth[1]]++;
 
-	int result = 1;
-	for(pair<string, int> cloth : divideCloth)
-		result *= (combination(cloth.second, 1) + 1);
+	long long result = 1;
+	//for(pair<string, int> cloth : divideCloth)
+	//	result *= (combination(cloth.second, 1) + 1);
 
-	return result-1;
-}
+	for (pair<string, int> cloth : divideCloth)
+		result = result * (cloth.second + 1);
 
-int main()
-{
-	vector<vector<string>> clothes = { {"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"} };
-	//vector<vector<string>> clothes = { {"crow_mask", "face"}, {"blue_sunglasses", "face"}, {"smoky_makeup", "face"} };
-
-	cout << solution(clothes) << endl;
-
-	cout << combination(2,1) << endl;
+	return result - 1;
 }
