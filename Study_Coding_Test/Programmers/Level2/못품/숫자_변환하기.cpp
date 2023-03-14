af1a3bd782d413986f9b5013155360a91fd2d5e1
cse-child <99868973+cse-child@users.noreply.github.com>
2023-03-14 23:29:01 +0900

    # 230314 study

---
+++ Study_Coding_Test/Programmers/Level2/못품/숫자_변환하기.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+int solution(int x, int y, int n) {
+	int answer = 0;
+
+	while (x != y)
+	{
+		if (y % (x * 2) == 0)
+			x *= 2;
+		else if (y % (x * 3) == 0)
+			x *= 3;
+		else if ((x + n) <= y)
+			x += n;
+		else
+			return -1;
+		answer++;
+	}
+
+	return answer;
+}
+
+int main()
+{
+	solution(10, 40, 5);
+}
--- Study_Coding_Test/Programmers/Level2/전화번호_목록.cpp
+++ Study_Coding_Test/Programmers/Level2/전화번호_목록.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <algorithm>
 #include <string>
 #include <vector>
@@ -6,10 +7,23 @@ using namespace std;
 
 // 효율성 테스트 계속 실패 -> 문자열 정렬을 하면 사전순으로 정렬된다
 bool solution(vector<string> phone_book) {
+	for (string b : phone_book)
+		cout << b << endl;
+
 	sort(phone_book.begin(), phone_book.end());
+
+	cout << endl;
+	for (string b : phone_book)
+		cout << b << endl;
 
 	for (int i = 0; i < phone_book.size() - 1; i++)
 		if (phone_book[i + 1].find(phone_book[i]) == 0) return false;
 
 	return true;
 }
+
+//int main()
+//{
+//	vector<string> v = { "119", "97674223", "1195524421" };
+//	solution(v);
+//}
