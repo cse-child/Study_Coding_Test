fd58c3d2d78e4527ff52b5e9ad3edeac2a88f612
cse-child <99868973+cse-child@users.noreply.github.com>
2023-05-01 21:33:38 +0900

    # 230501 study

---
+++ Study_Coding_Test/Programmers/Level2/소수_찾기.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+void DFS(string numbers, int index)
+{
+	
+}
+
+set<int> nums;
+int solution(string numbers) {
+	DFS(numbers, 0);
+
+	
+
+
+	return 0;
+}
+
+int main()
+{
+	string s = "01";
+	cout << stoi(s) << endl;;
+}
