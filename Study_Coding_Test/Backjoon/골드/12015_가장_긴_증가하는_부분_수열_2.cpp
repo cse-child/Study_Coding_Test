52f74e4ce1cc7cfb62e86a9ad1f3361ff37afe67
cse-child <99868973+cse-child@users.noreply.github.com>
2023-08-09 21:34:36 +0900

    # 230809 study - LIS 시간초과 해결

---
+++ Study_Coding_Test/Backjoon/골드/12015_가장_긴_증가하는_부분_수열_2.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> arr;
+
+int BinarySearch(int start, int end, int num)
+{
+	while(start < end)
+	{
+		int mid = (start + end) / 2;
+		if (num > arr[mid])
+			start = mid + 1;
+		else
+			end = mid;
+	}
+	return start;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	int N;
+	cin >> N;
+	vector<int> A(N);
+	for (int& a : A)
+		cin >> a;
+
+	arr.push_back(A[0]);
+	int cnt = 1;
+	for(int i = 1; i < N; i++)
+	{
+		if(A[i] > arr.back())
+		{
+			arr.push_back(A[i]);
+			cnt++;
+		}
+		else
+		{
+			int idx = BinarySearch(0, cnt, A[i]);
+			arr[idx] = A[i];
+		}
+	}
+	cout << cnt;
+
+	return 0;
+} 
