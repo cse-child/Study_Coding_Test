6debd6ffb9837741907088992d37fb831b2dc8b9
cse-child <dbswl9773@naver.com>
2023-09-18 21:30:00 +0900

    # 230918 study - Greedy연습

---
+++ Study_Coding_Test/Programmers/Level3/섬_연결하기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, vector<vector<int>> costs) {
+	int answer = 0;
+
+	for(int i = 0; i < costs.size(); i++)
+	{
+		for(int j = 0; j < costs[i].size(); j++)
+		{
+			
+		}
+	}
+
+	return answer;
+}
