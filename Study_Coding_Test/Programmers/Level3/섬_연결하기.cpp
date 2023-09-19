347a264c668cd2de01ccebea17b7366bc3849fc0
cse-child <dbswl9773@naver.com>
2023-09-19 21:30:39 +0900

    # 230919 study - MST 연습

--- Study_Coding_Test/Programmers/Level3/섬_연결하기.cpp
+++ Study_Coding_Test/Programmers/Level3/섬_연결하기.cpp
@@ -1,18 +1,57 @@
+#include <algorithm>
 #include <string>
 #include <vector>
 
 using namespace std;
 
+vector<int> parent(101);
+
+int Find(int x)
+{
+	if (x == parent[x])
+		return x;
+
+	return parent[x] = Find(parent[x]);
+}
+
+bool Union(int a, int b)
+{
+	a = Find(a);
+	b = Find(b);
+
+	if (a == b)
+		return false;
+
+	if (a > b)
+		parent[a] = b;
+	else
+		parent[b] = a;
+
+	return true;
+}
+
+// [0]-[1]: 연결된 노드, [2]:비용
 int solution(int n, vector<vector<int>> costs) {
 	int answer = 0;
 
+	for (int i = 1; i <= n; i++)
+		parent[i] = i;
+
+	sort(costs.begin(), costs.end(), [](const vector<int>& cost1, const vector<int>& cost2){
+		return cost1[2] < cost2[2];
+	});
+
 	for(int i = 0; i < costs.size(); i++)
 	{
-		for(int j = 0; j < costs[i].size(); j++)
-		{
-			
-		}
-	}
+		if (Union(costs[i][0], costs[i][1]))
+			answer += costs[i][2];
+;	}
 
 	return answer;
+}
+
+int main()
+{
+	vector<vector<int>> costs = { {0,1,1},{0,2,2},{1,2,5},{1,3,1},{2,3,8} };
+	solution(4, costs);
 }
