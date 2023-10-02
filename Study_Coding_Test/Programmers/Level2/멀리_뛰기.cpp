c023e106be967da2f5f719ddff8c6213f7fb5a60
cse-child <dbswl9773@naver.com>
2023-10-02 12:47:40 +0900

    # 231002 study - DP연습

--- Study_Coding_Test/Programmers/Level2/멀리_뛰기.cpp
+++ Study_Coding_Test/Programmers/Level2/멀리_뛰기.cpp
@@ -1,9 +1,30 @@
+#include <iostream>
 #include <string>
 #include <vector>
 
 using namespace std;
 
+int dp[2001];
+
 long long solution(int n) {
-    long long answer = 0;
-    return answer;
+	dp[1] = dp[2] = 1;
+
+	for(int i = 2; i <= n; i++)
+	{
+		dp[i] += (dp[i - 1] + dp[i - 2]) % 1234567;
+	}
+
+	return dp[n];
+}
+
+// 베스트 풀이..
+int Jump(int num)
+{
+	return num < 2 ? 1 : Jump(num - 1) + Jump(num - 2);
+}
+
+int main()
+{
+	cout << solution(3) << endl;
+	cout << Jump(4) << endl;
 }
