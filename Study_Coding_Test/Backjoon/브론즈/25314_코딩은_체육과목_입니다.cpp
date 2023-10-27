8e8e4a2e3776e0ae5f7d63480e63e944fc190372
cse-child <99868973+cse-child@users.noreply.github.com>
2023-10-27 23:31:53 +0900

    # 231027 study

--- Study_Coding_Test/Backjoon/브론즈/25314_코딩은_체육과목_입니다.cpp
+++ Study_Coding_Test/Backjoon/브론즈/25314_코딩은_체육과목_입니다.cpp
@@ -8,10 +8,13 @@ using namespace std;
 
 int main()
 {
-	int N;
-	cin >> N;
+    int n;
+    cin >> n;
 
+    for (int i = 0; i < n / 4; i++) {
+        cout << "long ";
+    }
+    cout << "int";
 
-
-	return 0;
+    return 0;
 }
