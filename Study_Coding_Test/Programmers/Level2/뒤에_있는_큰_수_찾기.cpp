302c4a17b6af2b45512c47e6780ecaeb1f6562f0
cse-child <dbswl9773@naver.com>
2023-12-06 21:27:49 +0900

    # 231206 study - Stack 연습

--- Study_Coding_Test/Programmers/Level2/뒤에_있는_큰_수_찾기.cpp
+++ Study_Coding_Test/Programmers/Level2/뒤에_있는_큰_수_찾기.cpp
@@ -36,30 +36,6 @@ vector<int> solution(vector<int> numbers) {
 	return answer;
 }
 
-vector<int> solution2(vector<int> numbers) {
-	vector<int> result(numbers.size());
-	vector<pair<int, int>> nums;
-	for (int i = 0; i < numbers.size(); i++)
-		nums.emplace_back(numbers[i], i);
-
-	// 9 1 5 3 6 2
-	// 0 1 2 3 4 5
-
-	// 1 2 3 5 6 9
-	// 1 5 3 2 4 0
-
-	sort(nums.begin(), nums.end());
-
-	for(int i = 0; i < numbers.size(); i++)
-	{
-		int idx = nums[i].second + 1;
-		int findIdx = find(nums.begin()->second, nums.end()->second, idx) - nums.begin()->second;
-		cout << findIdx << " ";
-	}
-	
-
-	return result;
-}
 
 int main()
 {
